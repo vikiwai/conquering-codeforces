de64b57350db98e5e223a4dbb03c7c209c2b9984
Victoria Bunyaeva <avikiwai@gmail.com>
2017-07-16 15:31:06 +0300

    Add a method for checking the palindrome

--- day1/src/CF419A.java
+++ day1/src/CF419A.java
@@ -2,6 +2,33 @@ import java.util.Scanner;
 
 public class CF419A
 {
+    public static boolean isPalindrome(int hours, int minutes){
+        String[] arrayTime = {"", ""};
+
+        if (hours < 10) {
+            arrayTime[0] = "0" + hours;
+        }
+        else {
+            arrayTime[0] = String.valueOf(hours);
+        }
+
+        if (minutes < 10) {
+            arrayTime[1] = "0" + minutes;
+        }
+        else {
+            arrayTime[1] = String.valueOf(minutes);
+        }
+
+        arrayTime[1] = new StringBuffer(arrayTime[1]).reverse().toString();
+
+        if (Integer.parseInt(arrayTime[0]) == Integer.parseInt(arrayTime[1])) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,10 +39,6 @@ public class CF419A
         int hours = Integer.parseInt(array[0]);
         int minutes = Integer.parseInt(array[1]);
 
-        System.out.print("hours = ");
-        System.out.println(hours);
-
-        System.out.print("minutes = ");
-        System.out.println(minutes);
+        System.out.println(isPalindrome(hours, minutes));
     }
 }
