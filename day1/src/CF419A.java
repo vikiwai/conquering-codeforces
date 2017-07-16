2acee00bd35d03555fdcc0735652021fb67a9e44
Victoria Bunyaeva <avikiwai@gmail.com>
2017-07-16 15:01:39 +0300

    Implement input of data

--- day1/src/CF419A.java
+++ day1/src/CF419A.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class CF419A
 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+
+        String[] array = line.split(":");
+
+        int hours = Integer.parseInt(array[0]);
+        int minutes = Integer.parseInt(array[1]);
+
+        System.out.print("hours = ");
+        System.out.println(hours);
+
+        System.out.print("minutes = ");
+        System.out.println(minutes);
     }
 }
