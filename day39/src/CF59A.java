7b10cd6425245f6928867b6ea28763cf73e8df75
Victoria Bunyaeva <avikiwai@gmail.com>
2017-08-23 15:07:59 +0300

    Implement data output

--- day39/src/CF59A.java
+++ day39/src/CF59A.java
@@ -33,5 +33,12 @@ public class CF59A
                 k -=1;
             }
         }
+        
+        if (k > 0) {
+            System.out.println(String.valueOf(line).toUpperCase());
+        }
+        else {
+            System.out.println(String.valueOf(line).toLowerCase());
+        }
     }
 }
