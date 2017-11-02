2632f85b568763d2d504ea008ffb3a26cef4970d
Victoria Bunyaeva <avikiwai@gmail.com>
2017-11-02 15:48:41 +0300

    Add levota

--- day47/src/CF455A.cpp
+++ day47/src/CF455A.cpp
@@ -23,6 +23,41 @@ int main()
     for(int i = 0; i < n; ++i) {
         std::cin >> array[i];
     }
+
+    int output = 0;
+    int max = -1;
+    bool flag = true;
+
+    while (flag) {
+
+        for (int j = 0; j < n; ++j) {
+            if (array[j] > max) {
+                max = array[j];
+            }
+        }
+
+        for (int j = 0; j < n; ++j) {
+            if (array[j] == max || array[j] == (max + 1) || array[j] == (max - 1)) {
+                array[j] = -1;
+            }
+        }
+
+        output++;
+
+        for(int i = n; i > 0; i--) {
+            for(int j = 0; j < i - 1; j++) {
+                if(array[j] >= array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        if(array[n - 1] == -1) {
+            flag = false;
+        }
+    }
     
     return 0;
 }
