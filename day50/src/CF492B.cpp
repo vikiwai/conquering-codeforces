5a2cb3d4da36c5885a7549af07f48fa3ba59dd8e
Victoria Bunyaeva <avikiwai@gmail.com>
2017-11-05 18:42:17 +0300

    Add the solution

--- day50/src/CF492B.cpp
+++ day50/src/CF492B.cpp
@@ -27,5 +27,26 @@ int main()
         std::cin >> arr[i];
     }
 
+    for(int i = n; i > 0; i--) {
+        for(int j = 0; j < i - 1; j++) {
+            if(arr[j] >= arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    int max = 0;
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        if(arr[i + 1] - arr[i] > max) {
+            max = arr[i + 1] - arr[i];
+        }
+    }
+
+    std::cout << l / max << std::endl;
+
     return 0;
 }
