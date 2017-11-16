e9c93c323bb3db4b2f231d3670b0e2ce1f3461be
Victoria Bunyaeva <avikiwai@gmail.com>
2017-11-17 01:31:05 +0300

    Add full solution

--- day55/src/CF478A.cpp
+++ day55/src/CF478A.cpp
@@ -19,5 +19,17 @@
 
 int main()
 {
+    int c1, c2, c3, c4, c5;
+    std::cin >> c1 >> c2 >> c3 >> c4 >> c5;
+
+    int sum = c1 + c2 + c3 + c4 + c5;
+
+    if(sum % 5 == 0 && sum != 0) {
+        std::cout << sum / 5;
+    }
+    else {
+        std::cout << "-1";
+    }
+
     return 0;
 }
